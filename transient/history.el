7d93402ac9c0f7037c6975be0cd29b62d9f912ac
smiking <fuck@aa.com>
2021-01-26 01:45:44 +0800

    update .gitignore

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-branch nil)
- (magit-commit nil)
- (magit-dispatch nil)
- (magit-log
-  ("--graph" "--color" "--decorate" "++header" "--patch" "--stat")
-  ("-n256" "--graph" "--decorate")
-  ("-n256" "--topo-order" "--graph" "--decorate" "--show-signature" "--patch")
-  ("-n256" "--decorate"))
- (magit-stash nil))
